d04b34634f185feca663b80dab00b162fd39e0e4
briggsz07 <briggsz@wlhs.wlwv.k12.or.us>
2025-05-05 09:42:18 -0700

    commit

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-def main():
-    pass
+# def main():
+    # pass
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+    # main()
+
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("F1 hotlap") # geeksforgeeks (7-24)
+
+
+from pygame.locals import *
+
+# create the display surface object
+# of specific dimension.
+window = pygame.display.set_mode((600, 600))
+
+# Fill the scree with white color
+window.fill((255, 255, 255))
+
+# Using draw.rect module of
+# pygame to draw the solid rectangle
+pygame.draw.rect(window, (0,   0, 200), # color
+                [50, 300, 75, 50], 10) # position, position, size, size
+
+# Draws the surface object to the screen.
+pygame.display.update()
+
+# Game loop
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Quit Pygame
+pygame.quit()
 
     
