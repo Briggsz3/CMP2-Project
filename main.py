e5992b436f48c76e1c638ab94855f23f8c44f3e3
briggsz07 <briggsz@wlhs.wlwv.k12.or.us>
2025-05-09 09:39:40 -0700

    commit

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-# def main():
-    # pass
-
-# if __name__ == '__main__':
-    # main()
-
 import pygame
+from pygame.locals import *
+
+f1_logo = pygame.image.load("./images/f1.256px.png")  # from Code with prince, YT video
+pygame.display.set_icon(f1_logo)
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the game window
-screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("F1 hotlap") # geeksforgeeks (7-24)
+screen = pygame.display.set_mode((1000, 750))
+pygame.display.set_caption("Silverstone hotlap") # geeksforgeeks (7-24)
 
 
-from pygame.locals import *
+# load f1 images
+f1_car = pygame.image.load("./images/racing-car.png")
+car_x = 500
+car_y = 375
+
+def car():
+    screen.blit(f1_car,(car_x, car_y))
 
 # create the display surface object
 # of specific dimension.
-window = pygame.display.set_mode((600, 600))
+window = pygame.display.set_mode((1500, 750))
 
 # Fill the scree with white color
 window.fill((255, 255, 255))
@@ -27,6 +31,7 @@ window.fill((255, 255, 255))
 # pygame to draw the solid rectangle
 pygame.draw.rect(window, (0,   0, 200), # color
                 [50, 300, 75, 50], 10) # position, position, size, size
+
 
 # Draws the surface object to the screen.
 pygame.display.update()
@@ -38,6 +43,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
 
 # Quit Pygame
 pygame.quit()
