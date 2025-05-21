1bc738f85432ab6fc11edcad22072bc3e7e0c9d8
briggsz07 <briggsz@wlhs.wlwv.k12.or.us>
2025-05-21 11:20:58 -0700

    commit

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ pygame.display.set_caption("Silverstone hotlap") # geeksforgeeks (7-24)
 
 # load f1 images
 f1_car = pygame.image.load("./images/racing-car.png")
-car_x = 500
-car_y = 375
+car_x = 275
+car_y = 600
 
 
 
@@ -33,7 +33,8 @@ pygame.draw.rect(window, (0,   0, 200), # color
                 [50, 300, 75, 50], 10) # position, position, size, size
 
 
-
+# background
+background = pygame.image.load("./images/trackv2.webp") # buildwithpython - pygame tutorial -10-
 
 
 # Draws the surface object to the screen.
@@ -50,30 +51,35 @@ angle = 0
 
 # infinite loop 
 while running: 
+	window.fill((0,0,0))
+	window.blit(background, (10,10))
 	for game_event in pygame.event.get():
 		if game_event.type == pygame.QUIT:
 			running = False
-		
-		pygame.time.delay(10) 
+			pygame.time.delay(10) 
 	
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT: 
 			running = False
 	keys = pygame.key.get_pressed() 
 	
-	if keys[pygame.K_LEFT]: 
-		angle+=6
-		f1_car = pygame.transform.rotate(f1_car, angle)
-		
-	if keys[pygame.K_RIGHT]: 
-		angle-=6
-		f1_car = pygame.transform.rotate(f1_car, angle)
+	if keys[pygame.K_LEFT] and car_x>0: 
+		car_x -= vel/50
+
+	if keys[pygame.K_RIGHT] and car_x<1500-width: 
+		car_x += vel/50
+
 		
 	if keys[pygame.K_UP] and car_y>0: 
-		car_y -= vel/10
+		car_y -= vel/50
 		
 	if keys[pygame.K_DOWN] and car_y<750-height: 
-		car_y += vel/10
+		car_y += vel/50
+	
+	if keys[pygame.K_a]:
+		angle +=6
+		f1_car = pygame.transform.rotate(f1_car, angle)
+		f1_car = pygame.image.load("./images/racing-car.png")
 	window.blit(f1_car,(car_x, car_y))
 	pygame.display.flip()
 	pygame.display.update()
