18925f39fc396ecffa1a61ea5ff383f012ca60ff
briggsz07 <briggsz@wlhs.wlwv.k12.or.us>
2025-05-30 09:10:35 -0700

    commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from pygame.locals import *
+import math 
+
+
 
 f1_logo = pygame.image.load("./images/f1.256px.png")  # from Code with prince, YT video
 pygame.display.set_icon(f1_logo)
@@ -8,12 +11,14 @@ pygame.display.set_icon(f1_logo)
 pygame.init()
 
 # Set up the game window
-pygame.display.set_caption("Silverstone hotlap") # geeksforgeeks (7-24)
-
+pygame.display.set_caption("F1 hotlap") # geeksforgeeks (7-24)
 
 # load f1 images
 f1_car = pygame.image.load("./images/racing-car.png")
-car_x = 275
+f1_w = f1_car.get_width()
+f1_h = f1_car.get_height()
+f1_car = pygame.transform.scale(f1_car,(f1_w*1.25, f1_h*1.25))
+car_x = 540
 car_y = 600
 
 
@@ -24,17 +29,13 @@ car_y = 600
 # of specific dimension.
 window = pygame.display.set_mode((1500, 750))
 
-# Fill the scree with white color
-window.fill((255, 255, 255))
-
-# Using draw.rect module of
-# pygame to draw the solid rectangle
-pygame.draw.rect(window, (0,   0, 200), # color
-                [50, 300, 75, 50], 10) # position, position, size, size
-
 
 # background
 background = pygame.image.load("./images/trackv2.webp") # buildwithpython - pygame tutorial -10-
+w = background.get_width()
+h = background.get_height() # Coding with Russ
+background = pygame.transform.scale(background,(w*2,h))
+
 
 
 # Draws the surface object to the screen.
@@ -51,6 +52,7 @@ angle = 0
 
 # infinite loop 
 while running: 
+	# generates the background
 	window.fill((0,0,0))
 	window.blit(background, (10,10))
 	for game_event in pygame.event.get():
@@ -63,24 +65,29 @@ while running:
 			running = False
 	keys = pygame.key.get_pressed() 
 	
+	# make the image rotate
 	if keys[pygame.K_LEFT] and car_x>0: 
-		car_x -= vel/50
+		angle += (1/2)
+		surf = pygame.transform.rotate(f1_car, angle)
 
-	if keys[pygame.K_RIGHT] and car_x<1500-width: 
-		car_x += vel/50
 
-		
+	if keys[pygame.K_RIGHT] and car_x<1500-width:
+		angle -= (1/2)
+		surf = pygame.transform.rotate(f1_car, angle) # YouTube video
+
+	
+	
 	if keys[pygame.K_UP] and car_y>0: 
-		car_y -= vel/50
+		car_x += (vel/15)*math.cos(math.radians(angle))  # changes the x and y coordinates at the same time to make the car go forward
+		car_y -= (vel/15)*math.sin(math.radians(angle)) # YouTube video
 		
 	if keys[pygame.K_DOWN] and car_y<750-height: 
-		car_y += vel/50
-	
-	if keys[pygame.K_a]:
-		angle +=6
-		f1_car = pygame.transform.rotate(f1_car, angle)
-		f1_car = pygame.image.load("./images/racing-car.png")
-	window.blit(f1_car,(car_x, car_y))
+		car_x -= (vel/25)*math.cos(math.radians(angle))
+		car_y += (vel/25)*math.sin(math.radians(angle))
+			
+	# generate the image
+	surf = pygame.transform.rotate(f1_car, angle)
+	window.blit(surf,(car_x,car_y))
 	pygame.display.flip()
 	pygame.display.update()
 
